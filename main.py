ea7a537546835c0e522c65fd04adc27c257d4f08
Makar-aka <moptuh@yandex.ru>
2025-05-25 22:22:35 +0300

    Изменен импорт для рендеринга шаблонов Flask
    Обновлен импорт с `render_template_string` на `render_template` для улучшения читаемости и поддержки кода.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response, render_template_string, redirect, url_for
+from flask import Flask, request, Response, render_template, redirect, url_for
 from tracker import *
 from db_handlers import SQLiteCommon
 import configparser
